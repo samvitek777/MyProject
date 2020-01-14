847c95e365e8b69108f183f7b133390f79f57d12
Victor Samoylenko <samvitek1@gmail.com>
2020-01-14 17:53:09 +0300

    two day add login and reg page + db

--- src/main/java/com/example/myproject/controller/LoginAndRegisterController.java
+++ src/main/java/com/example/myproject/controller/LoginAndRegisterController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /** Controller for {@link User} login and registration
  *
@@ -24,19 +26,29 @@ public class LoginAndRegisterController {
         return "login";
     }
 
-    //Какая - то ебаная ошибка
-
-  /*  @PostMapping("login")
-    public String login(@RequestParam("login") String login,
-            @RequestParam("password") String password){
-        System.out.println(login + " " + password);
-        return "login";
-    }*/
+    @PostMapping("login")
+    public String login1(@RequestParam String username,
+                         @RequestParam String password){
+        System.out.println(username + " " + password);
+        return "redirect:";
+    }
 
     @GetMapping("registration")
     public String registration( Model model){
         User user = userRepo.findByUsername("admin");
         model.addAttribute("id", user.getId());
+        System.out.println(user.getId());
         return "registration";
+    }
+
+    @PostMapping("registration")
+    public String addUser(User user, Model model){
+        if(userRepo.findByUsername(user.getUsername()) != null){
+            model.addAttribute("user", user.getUsername());
+            return "test";
+        } else {
+            userRepo.save(user);
+            return "redirect:";
+        }
     }
 }
