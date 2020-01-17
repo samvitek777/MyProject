6d2270bc5320243c7aff5b1c505f3e1f1b77c8aa
Victor Samoylenko <samvitek1@gmail.com>
2020-01-17 12:43:06 +0300

    add resourses ignor security

--- src/main/java/com/example/myproject/config/WebSecurityConfig.java
+++ src/main/java/com/example/myproject/config/WebSecurityConfig.java
@@ -25,7 +25,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                     //antMatchers(“...”).permitAll() данные urls защищать не надо
-                    .antMatchers("/resources/**","/", "/registration").permitAll()  //antMatchers(“...”).permitAll() данные urls защищать не надо
+                    .antMatchers("/resources/**","/", "/registration", "/libs/bootstrap.min.css",
+                            "/css/index.css", "/css/register.css", "/img/baraholka.png").permitAll()  //antMatchers(“...”).permitAll() данные urls защищать не надо
                     .anyRequest().authenticated()
                     .and()
                 //loginPage(“/login”) определяется страница для совершения логина в приложение, которая доступна всем
